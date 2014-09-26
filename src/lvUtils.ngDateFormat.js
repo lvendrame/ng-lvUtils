f159807cceb631f4c50f90e696eeb53dcdf2168d
unknown <luis.vendrame@WKSDIGI289.digisystem.corp>
2014-09-26 18:41:30 -0300

    ngDateFormat

---
+++ src/lvUtils.ngDateFormat.js
@@ -0,0 +1,16 @@
+﻿lvUtils.directive("ngFormat", ['dateFilter', function (dateFilter) {
+	return {
+		require: '^ngModel',
+		restrict: 'A',
+		link: function (scope, elm, attrs, ctrl) {
+			var dateFormat = attrs.ngFormat;
+			var model = attrs.ngModel;
+
+			scope.$watch(model, function (value) {
+				var newValue = dateFilter(value, dateFormat);
+				ctrl.$setViewValue(newValue);
+				ctrl.$render();
+			});
+		}
+	};
+}]);
