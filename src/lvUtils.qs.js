0d221176df52834798134266d9e92f5d1a289997
Luís Fernando Vendrame <lvendrame@gmail.com>
2020-11-19 11:44:56 +0000

    Update lvUtils.qs.js

--- src/lvUtils.qs.js
+++ src/lvUtils.qs.js
@@ -1,12 +1,11 @@
-lvUtils.factory('$qs', ['$window', function ($window) {
-    var qs;
+lvUtils.factory('$qs', ['$window', function ($window) {    
     if ($window.location.search) {
-        qs = $window.location.search.substr(1).split("&").reduce(function (previousValue, currentValue, index, array) {
+        return $window.location.search.substr(1).split("&").reduce(function (previousValue, currentValue, index, array) {
             var aux = currentValue.split('=');
             previousValue[aux[0]] = decodeURIComponent(aux[1].replace(/\+/g, " "));
             return previousValue;
         }, {});
-    } else { qs = {}; }
+    }
 
-    return qs;
+    return {};
 }]);
